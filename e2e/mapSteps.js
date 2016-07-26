053bc454434d2bb893fd713b2a8a5cdcc31c4227
Panos Terzis <panagiwtis.terzis@gmail.com>
2016-07-26 08:24:20 +0100

    fix test for data

--- e2e/mapSteps.js
+++ e2e/mapSteps.js
@@ -17,18 +17,14 @@ var mapSteps = function() {
 
   this.Then(/^the map should contain data$/, function(callback){
 		browser.driver.sleep(2000);
-    expect(browser.driver.executeScript(function(browser) {
+    browser.driver.executeScript(function(browser) {
       var canvas = document.getElementsByTagName('canvas')[0];
       var data = canvas.getContext('2d').getImageData(0,0,canvas.width,canvas.height).data;
-			console.log(data)
-			console.log(data.length > 0)
-			console.log(data[4] > 0)
-			console.log(data.length > 0 && data[4] > 0)
       return data.length > 0 && data[4] > 0;
-			//expect(data[4]).to.be.above(0);
-			//console.log('xxxxxxxx')
-			//callback();
-		})).to.eventually.equal(true);
+		}).then(function(data){
+			expect(data).to.equal(true);
+			callback();
+		})
 		//browser.pause();
   });
 
